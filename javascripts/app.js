6590b30fec0d85d1bd0efe99d4e166ab177c0b2f
mkSunda <mksunda01@gmail.com>
2013-05-17 21:20:14 +0530

    Create app.js

---
+++ javascripts/app.js
@@ -0,0 +1,34 @@
+ function check(i)  {
+              switch(i)
+  		  {   case 0:
+                  document.getElementById("collar").src = "img/collar_normal_longsleeve.png";
+				  break;
+                 case 1:
+                  document.getElementById("collar").src = "img/collar_normal_cutaway.png";
+				  break ;
+				  
+				   case 2:
+               document.getElementById("collar").src = "img/collar_ladies_normal.png";
+			    break; 
+				
+				 case 3:
+                  document.getElementById("cuff").src = "img/cuff4.png";
+				  break;
+                 case 4:
+                  document.getElementById("cuff").src = "img/cuff2.png";
+				  break ;
+				  
+				   case 5:
+               document.getElementById("cuff").src = "img/cuff1.png";
+			    break; 
+				
+				case 6:
+                  document.getElementById("button").src = "img/button2.png";
+				  break ;
+				  
+				   case 7:
+               document.getElementById("button").src = "img/button1.png";
+			    break; 
+				 
+              }
+          }
